10ca7f30c6aba30a6d6bf7ca9028645ee5b7555a
cpthgli <cpthgli@gmail.com>
2016-07-02 00:11:23 +0900

    型変換の修正と永続再生できるように変更

--- binauralBeat.py
+++ binauralBeat.py
@@ -6,7 +6,7 @@ class StereoSound:
 
     def __init__(self, format=pa.paInt16, sampling=44100):
         self.form = format
-        self.samp = int(sampling)
+        self.samp = sampling
 
     def generateSinWave(self, left, right, volume=0.98, time=1.0):
         cast = {pa.paFloat32:(float, 1.0, 'f'),
@@ -17,8 +17,8 @@ class StereoSound:
         length = int(time * self.samp)
         num = length << 1
         data = [0.0] * num
-        left_theta = 2 * pi * left / self.samp
-        right_theta = 2 * pi * right / self.samp
+        left_theta = 2 * pi * float(left) / self.samp
+        right_theta = 2 * pi * float(right) / self.samp
         for (n, l), r in zip(enumerate(range(0, num,2)), range(1, num,2)):
             data[l] = volume * sin(left_theta * n)
             data[r] = volume * sin(right_theta * n)
@@ -29,17 +29,17 @@ class StereoSound:
         p = pa.PyAudio()
         stream = p.open(format=self.form,
                         channels=2,
-                        rate=self.samp,
+                        rate=int(self.samp),
                         output=True)
-        while repeat > 0:
+        while repeat != 0:
             stream.write(self.data)
             repeat -= 1
         stream.close()
         p.terminate()
 
 if __name__ == "__main__" :
-    sound = StereoSound(pa.paFloat32, 192000)
+    sound = StereoSound()
     f1 = 440
     f2 = 450
-    sound.generateSinWave(f1, f2, 0.05)
-    sound.play(5)
+    sound.generateSinWave(f1, f2, 0.002)
+    sound.play(-1)
