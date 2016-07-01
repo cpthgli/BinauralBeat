87e9be8650b6e1a6d02370bbdd99142f56e3be13
cpthgli <cpthgli@gmail.com>
2016-07-01 16:57:34 +0900

    倍精度以外のsin波生成時に発生する不具合の修正

--- binauralBeat.py
+++ binauralBeat.py
@@ -1,37 +1,38 @@
 #coding: utf-8
-import struct, pyaudio as pa
+import sys, struct, wave, pyaudio as pa
 from math import sin, pi
 
 class StereoSound:
 
-    def __init__(self, format, rate):
+    def __init__(self, format=pa.paInt16, sampling=44100, bit=16):
         self.format = format
-        self.rate = rate
+        self.sampling = int(sampling)
         self.cast = {
-            pa.paFloat32:(float,'f'),
-            pa.paInt32:(int,'q'),
-            pa.paInt24:(int,'3b'),
-            pa.paInt16:(int,'i'),
-            pa.paInt8:(int,'h'),
-            pa.paUInt8:(int,'I'),
+            pa.paFloat32:(float,2,'f'),
+            pa.paInt32:(int,32,'i'),
+            pa.paInt24:(int,24,'3b'),
+            pa.paInt16:(int,16,'h'),
+            pa.paInt8:(int,8,'b'),
         }
+        self.bit = int(bit)
 
-    def generateSinWave(self, left, right, volume=1, time=1):
-        length = time * self.rate
-        data = [0.0]*(length<<1)
-        left_theta = 2 * pi * left / self.rate
-        right_theta = 2 * pi * right / self.rate
+    def generateSinWave(self, left, right, volume=0.98, time=1.0):
+        length = int(time * self.sampling)
+        d = [0.0]*(length<<1)
+        left_theta = 2 * pi * left / self.sampling
+        right_theta = 2 * pi * right / self.sampling
         for n, l, r in zip(range(0,length),range(0,length<<1,2),range(1,length<<1,2)):
-            data[l] = volume * sin(left_theta * n)
-            data[r] = volume * sin(right_theta * n)
-        data = [self.cast[self.format][0](x) for x in data]
-        self.data = struct.pack(self.cast[self.format][1] * (length<<1), *data)
+            d[l] = volume * sin(left_theta * n)
+            d[r] = volume * sin(right_theta * n)
+        d = [self.cast[self.format][0](x * float(2**(self.cast[self.format][1]-1)-1)) for x in d]
+        self. d = d
+        self.data = struct.pack(self.cast[self.format][2] * (length<<1), *d)
 
     def play(self, repeat=1):
         p = pa.PyAudio()
         stream = p.open(format=self.format,
                         channels=2,
-                        rate=self.rate,
+                        rate=self.sampling,
                         output=True)
         while repeat>0:
             stream.write(self.data)
@@ -40,6 +41,8 @@ class StereoSound:
         p.terminate()
 
 if __name__ == "__main__" :
-    sound = StereoSound(pa.paFloat32, 192000)
-    sound.generateSinWave(440, 450, 0.05)
-    sound.play(3)
+    sound = StereoSound(pa.paInt16, 44100)
+    f1 = 440
+    f2 = 450
+    sound.generateSinWave(f1, f2, 0.5)
+    sound.play(10)
