9d00aef1e8f1afef80701b97f0683a422fa0a1d3
cpthgli <cpthgli@gmail.com>
2016-07-01 17:41:41 +0900

    フォーマットの統一とコードの短縮

--- binauralBeat.py
+++ binauralBeat.py
@@ -4,45 +4,42 @@ from math import sin, pi
 
 class StereoSound:
 
-    def __init__(self, format=pa.paInt16, sampling=44100, bit=16):
-        self.format = format
-        self.sampling = int(sampling)
-        self.cast = {
-            pa.paFloat32:(float,2,'f'),
-            pa.paInt32:(int,32,'i'),
-            pa.paInt24:(int,24,'3b'),
-            pa.paInt16:(int,16,'h'),
-            pa.paInt8:(int,8,'b'),
-        }
-        self.bit = int(bit)
+    def __init__(self, format=pa.paInt16, sampling=44100):
+        self.form = format
+        self.samp = int(sampling)
 
     def generateSinWave(self, left, right, volume=0.98, time=1.0):
-        length = int(time * self.sampling)
-        d = [0.0]*(length<<1)
-        left_theta = 2 * pi * left / self.sampling
-        right_theta = 2 * pi * right / self.sampling
-        for n, l, r in zip(range(0,length),range(0,length<<1,2),range(1,length<<1,2)):
-            d[l] = volume * sin(left_theta * n)
-            d[r] = volume * sin(right_theta * n)
-        d = [self.cast[self.format][0](x * float(2**(self.cast[self.format][1]-1)-1)) for x in d]
-        self. d = d
-        self.data = struct.pack(self.cast[self.format][2] * (length<<1), *d)
+        cast = {pa.paFloat32:(float, 1.0, 'f'),
+                pa.paInt32:(int, 2147483647.0, 'i'),
+                pa.paInt24:(int, 8388607.0, '3b'),
+                pa.paInt16:(int, 32767.0, 'h'),
+                pa.paInt8:(int, 127.0, 'b'),}
+        length = int(time * self.samp)
+        num = length << 1
+        data = [0.0] * num
+        left_theta = 2 * pi * left / self.samp
+        right_theta = 2 * pi * right / self.samp
+        for (n, l), r in zip(enumerate(range(0, num,2)), range(1, num,2)):
+            data[l] = volume * sin(left_theta * n)
+            data[r] = volume * sin(right_theta * n)
+        data = [cast[self.form][0](x * cast[self.form][1]) for x in data]
+        self.data = struct.pack(cast[self.form][2] * num, *data)
 
     def play(self, repeat=1):
         p = pa.PyAudio()
-        stream = p.open(format=self.format,
+        stream = p.open(format=self.form,
                         channels=2,
-                        rate=self.sampling,
+                        rate=self.samp,
                         output=True)
-        while repeat>0:
+        while repeat > 0:
             stream.write(self.data)
-            repeat-=1
+            repeat -= 1
         stream.close()
         p.terminate()
 
 if __name__ == "__main__" :
-    sound = StereoSound(pa.paInt16, 44100)
+    sound = StereoSound(pa.paFloat32, 192000)
     f1 = 440
     f2 = 450
-    sound.generateSinWave(f1, f2, 0.5)
-    sound.play(10)
+    sound.generateSinWave(f1, f2, 0.05)
+    sound.play(5)
