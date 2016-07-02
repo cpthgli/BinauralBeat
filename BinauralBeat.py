87e8d72e45245f009086025545a26fb828e33f6e
Hikaru Watanabe <cpthgli@gmail.com>
2016-07-03 01:47:31 +0900

    Formatted

--- BinauralBeat.py
+++ BinauralBeat.py
@@ -1,6 +1,9 @@
-#coding: utf-8
-import sys, struct, pyaudio as pa
+# coding: utf-8
+import sys
+import struct
+import pyaudio as pa
 from math import sin, pi
+
 
 class StereoSound:
 
@@ -9,17 +12,17 @@ class StereoSound:
         self.samp = sampling
 
     def generateSinWave(self, left, right, volume=0.98, time=1.0):
-        cast = {pa.paFloat32:(float, 1.0, 'f'),
-                pa.paInt32:(int, 2147483647.0, 'i'),
-                pa.paInt24:(int, 8388607.0, '3b'),
-                pa.paInt16:(int, 32767.0, 'h'),
-                pa.paInt8:(int, 127.0, 'b'),}
+        cast = {pa.paFloat32: (float, 1.0, 'f'),
+                pa.paInt32: (int, 2147483647.0, 'i'),
+                pa.paInt24: (int, 8388607.0, '3b'),
+                pa.paInt16: (int, 32767.0, 'h'),
+                pa.paInt8: (int, 127.0, 'b'), }
         length = int(time * self.samp)
         num = length << 1
         data = [0.0] * num
         left_theta = 2 * pi * float(left) / self.samp
         right_theta = 2 * pi * float(right) / self.samp
-        for (n, l), r in zip(enumerate(range(0, num,2)), range(1, num,2)):
+        for (n, l), r in zip(enumerate(range(0, num, 2)), range(1, num, 2)):
             data[l] = volume * sin(left_theta * n)
             data[r] = volume * sin(right_theta * n)
         data = [cast[self.form][0](x * cast[self.form][1]) for x in data]
@@ -37,7 +40,7 @@ class StereoSound:
         stream.close()
         p.terminate()
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     sound = StereoSound()
     if len(sys.argv) > 2:
         f1 = sys.argv[1]
